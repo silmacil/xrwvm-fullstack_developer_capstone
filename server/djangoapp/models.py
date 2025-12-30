eb9b55d602f1c0d0868087e222230fc940be9623
Sean M <silmacil@gmail.com>
2025-12-30 16:45:31 -0500

    .

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -30,7 +30,7 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
